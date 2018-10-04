b061a8fb11413171f5f43644316e0f7f718c5f58
Matthew Petroff <matthew@mpetroff.net>
2018-10-03 20:23:53 -0400

    When setting view via API, reset timer for auto rotate (fixes #664).

--- src/js/pannellum.js
+++ src/js/pannellum.js
@@ -2436,6 +2436,7 @@ this.setPitch = function(pitch, animated, callback, callbackArgs) {
     } else {
         config.pitch = pitch;
     }
+    latestInteraction = Date.now();
     animateInit();
     return this;
 };
@@ -2504,6 +2505,7 @@ this.setYaw = function(yaw, animated, callback, callbackArgs) {
     } else {
         config.yaw = yaw;
     }
+    latestInteraction = Date.now();
     animateInit();
     return this;
 };
@@ -2565,6 +2567,7 @@ this.setHfov = function(hfov, animated, callback, callbackArgs) {
     } else {
         setHfov(hfov);
     }
+    latestInteraction = Date.now();
     animateInit();
     return this;
 };
