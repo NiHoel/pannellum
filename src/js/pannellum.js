1631643e375889145fc3d6e3734c41b7bee03221
Matthew Petroff <matthew@mpetroff.net>
2018-12-21 16:40:02 -0500

    Add check for `document.fullscreenElement` since `document.fullscreen` is deprecated.

--- src/js/pannellum.js
+++ src/js/pannellum.js
@@ -2163,7 +2163,7 @@ function toggleFullscreen() {
  * @private
  */
 function onFullScreenChange(resize) {
-    if (document.fullscreen || document.mozFullScreen || document.webkitIsFullScreen || document.msFullscreenElement) {
+    if (document.fullscreenElement || document.fullscreen || document.mozFullScreen || document.webkitIsFullScreen || document.msFullscreenElement) {
         controls.fullscreen.classList.add('pnlm-fullscreen-toggle-button-active');
         fullscreenActive = true;
     } else {
