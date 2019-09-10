7c051e912e29f1c647621bd6ba8d15432750a879
Matthew Petroff <matthew@mpetroff.net>
2019-09-09 20:12:42 -0400

    Allow touch scrolling when `dragable` is false (fixes #788).

--- src/js/pannellum.js
+++ src/js/pannellum.js
@@ -959,6 +959,9 @@ var pointerIDs = [],
  */
 function onDocumentPointerDown(event) {
     if (event.pointerType == 'touch') {
+        // Only do something if the panorama is loaded
+        if (!loaded || !config.draggable)
+            return;
         pointerIDs.push(event.pointerId);
         pointerCoordinates.push({clientX: event.clientX, clientY: event.clientY});
         event.targetTouches = pointerCoordinates;
@@ -974,6 +977,8 @@ function onDocumentPointerDown(event) {
  */
 function onDocumentPointerMove(event) {
     if (event.pointerType == 'touch') {
+        if (!config.draggable)
+            return;
         for (var i = 0; i < pointerIDs.length; i++) {
             if (event.pointerId == pointerIDs[i]) {
                 pointerCoordinates[i].clientX = event.clientX;
